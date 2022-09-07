d68147d931bc557e2c4eda6ac37721fe7d0e0099
Oriko Tanaka <orikotanaka@OrikonoMacBook-Air.local>
2022-09-07 16:44:50 +0900

    ユーザー一覧　ユーザー編集画面にて格闘中　9/7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220830023212) do
+ActiveRecord::Schema.define(version: 20220906032105) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20220830023212) do
     t.string "department"
     t.datetime "basic_time", default: "2022-08-29 23:00:00"
     t.datetime "work_time", default: "2022-08-29 22:30:00"
+    t.integer "employee_nunmber"
+    t.string "uid"
+    t.datetime "basic_work_time", default: "2022-09-05 23:00:00"
+    t.datetime "designated_work_start_time"
+    t.datetime "designated_work_end_time"
+    t.boolean "superior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
