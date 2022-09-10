01250d159bb44d236dcb3d60bb8004df3e843de0
Oriko Tanaka <orikotanaka@OrikonoMacBook-Air.local>
2022-09-10 11:51:51 +0900

    csv,アコーディオン OK　9/10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220906032105) do
+ActiveRecord::Schema.define(version: 20220910020722) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,12 +20,12 @@ ActiveRecord::Schema.define(version: 20220906032105) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
-    t.string "department"
-    t.datetime "basic_time", default: "2022-08-29 23:00:00"
-    t.datetime "work_time", default: "2022-08-29 22:30:00"
+    t.string "affiliation"
+    t.datetime "basic_time", default: "2022-09-09 23:00:00"
+    t.datetime "work_time", default: "2022-09-09 22:30:00"
     t.integer "employee_nunmber"
     t.string "uid"
-    t.datetime "basic_work_time", default: "2022-09-05 23:00:00"
+    t.datetime "basic_work_time", default: "2022-09-09 23:00:00"
     t.datetime "designated_work_start_time"
     t.datetime "designated_work_end_time"
     t.boolean "superior", default: false
