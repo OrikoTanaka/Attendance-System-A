298d8f93004914e5cddd84d9f7ca033a2831825c
Oriko Tanaka <orikotanaka@OrikonoMacBook-Air.local>
2022-10-11 17:11:17 +0900

    拠点情報追加実装中　10/11

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,8 +1,29 @@
 class BasesController < ApplicationController
+  
+  def new
+    @base = Base.new
+  end
+
+  def create
+    @base = Base.new(base_params)
+    if @base.save
+      flash[:success] = "新規作成しました。"
+      redirect_to bases_url
+    else
+      render :new
+    end
+  end
+  
   def index
     @bases = Base.all
   end
 
   def edit
   end
+
+  private
+  
+    def base_params
+      params.require(:base).permit(:name, :base_number, :attendance_type)
+    end
 end
