aec42c86cfa082cf58b6bde963b04f94fa299e1e
Oriko Tanaka <orikotanaka@OrikonoMacBook-Air.local>
2022-10-24 15:25:02 +0900

    残業申請view作成中 10/24

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -6,8 +6,12 @@ class Attendance < ApplicationRecord
 
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
+  
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
+
+  # 出勤時間が存在、退勤時間が存在しない時、更新は無効
+  validate :started_at_is_invalid_withiout_a_finished_at, on: :update_one_month # update_one_monthのアクション実行の時のみバリデーションをかける
 
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
@@ -17,5 +21,9 @@ class Attendance < ApplicationRecord
     if started_at.present? && finished_at.present?
       errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
     end
+  end
+
+  def started_at_is_invalid_withiout_a_finished_at
+    errors.add(:finished_at, "が必要です") if started_at.present? && finished_at.blank?
   end
 end
